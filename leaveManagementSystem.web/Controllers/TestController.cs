6150c6408335906d8fda1207cf8696fc13f92ca8
USER <USER@CYBERSPACE>
2024-09-11 01:08:42 +0600

    Add project files.

---
+++ leaveManagementSystem.web/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using leaveManagementSystem.web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace leaveManagementSystem.web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Student",
+                DOB = new DateTime(1956,09,27)            };
+            return View(data);
+        }
+    }
+}
---
+++ leaveManagementSystem.web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace leaveManagementSystem.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
